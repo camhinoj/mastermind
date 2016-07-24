1ab2b4fd1425dedde2705e276d9b2a696c960be3
Cameron Hinojos <cameron.hinojos9@gmail.com>
2016-07-24 15:23:44 -0500

    Game works except for white, black

--- Mastermind.rb
+++ Mastermind.rb
@@ -1,40 +1,58 @@
 class Mastermind
+	attr_reader :computer
 
 	def initialize(player)
-		@name = Player.new(player)
-		@computer = Computer.new
-		@board = Board.new
+		@player = Player.new(player)
+		@answer = Computer.new.answer
+		@board = Board.new(@answer, @player)
+	end
+
+	def play
+		index = 1 
+		loop do
+			if index > 12
+				puts "You have lost the game"
+				break
+			end
+			puts "Enter your guess (four colors of red, green, yellow, purple, pink, or blue seperated by a comma, and the colors can repeat):"
+			guess = gets.chomp.split(', ')
+			if @board.add_guess(guess, index)
+				puts "You have won the game congrats"
+				break
+			else
+				@board.display
+			end
+			index += 1
+		end
 	end
 
 
 	Player = Struct.new(:name)
 
+
 	class Computer
 		attr_reader :answer
-		@answer = [0, 0, 0, 0]
 
-		def inititialize
+		def initialize
+			@colors = { "1" => "red", "2" => "green", "3" => "yellow", "4" => "purple", "5" => "pink", "6" => "blue" }
+			@answer = []
 			create_answer
 		end
 
 		def create_answer
-			@answer.collect! do |item|
-				rand(7)
-			end		
+			4.times do
+				@answer << @colors[rand(1..6).to_s]
+			end
+			p @answer
 		end
 	end
 
 	class Board
-		@colors = {
-			red: 31,
-			green: 32,
-			yellow: 33,
-			purple: 34,
-			pink: 35,
-			blue: 36,
-		}
+		attr_reader :answer
 
-		def initialize
+		def initialize(answer, player)
+			@answer = answer
+			@player = player
 			create_rows
 			display
 		end
@@ -44,11 +62,8 @@ class Mastermind
 			@rows = []
 			i = 12
 			12.times do
-				#@rows << ("row" + i.to_s).intern
 				@rows << sample_row
-				#i -= 1
 			end
-			#p @rows
 		end
 
 		# def init_display
@@ -61,9 +76,9 @@ class Mastermind
 
 
 		def display
-			@rows.each do |row|
+			@rows.reverse.each do |row|
 				puts "----------------------------------------------------------------------------\n" +
-				  	   "|\t#{color("red")}\t|\t#{row[1]}\t|\t#{row[2]}\t|\t#{row[3]}\t#{row[4]}\n|\t\s\t|\s\t\t|\t\s\t|\t\s\t#{row[5]}"
+				  	   "|\t#{color(row[0])}\t|\t#{color(row[1])}\t|\t#{color(row[2])}\t|\t#{color(row[3])}\t#{row[4]}\n|\t\s\t|\s\t\t|\t\s\t|\t\s\t#{row[5]}"
 			end
 		end
 		
@@ -90,11 +105,44 @@ class Mastermind
 			end
 		end
 
+		def add_guess(guess, turn)
+			@rows[turn-1] = guess
+			black_white = check_guess(guess)
+			if black_white == true
+				return true
+			else
+				@rows[turn-1] << "black:#{black_white[0]}"
+				@rows[turn-1] << "white:#{black_white[1]}"
+				return false
+			end
+		end
 
+		def check_guess(guess)
+			if guess == @answer
+				return true
+			end
+			black = 0
+			white = 0
+			@answer.each do |i|
+				guess.each do |j|
+					white += 1 if i == j
+				end
+			end
+			index = 0
+			while index < 3
+				if @answer[index] == guess[index]
+					black += 1
+					white -= 1
+				end
+				index += 1
+			end
+			return [black, white]
+		end
 	end
 end
 
-game = Mastermind.new("Cam")		
+game = Mastermind.new("Cam")	
+game.play
 
 
 	# @rows.each do |row|
